834b9c5f24eb5d71d0d0e9ce3fade9583cea576a
MrMelon54 <github@mrmelon54.com>
2023-12-27 20:10:32 +0000

    New randomisation algorithm

--- shuffle.go
+++ shuffle.go
@@ -1,17 +1,59 @@
 package main
 
-import "math/rand"
+import (
+	"math/rand"
+	"sort"
+)
 
 func ShuffledIntSlice(l int, seed int64) []int {
 	a := make([]int, l)
+	for i := range a {
+		a[i] = i
+	}
 	r := rand.New(rand.NewSource(seed))
 	for i := 0; i < l; i++ {
 		// use bound [1,l) so 0 can be used if the integer matches
 		n := r.Intn(l-1) + 1
-		if n == i {
+		if i == a[n] || a[i] == n {
 			n = 0
 		}
-		a[i] = n
+		a[n], a[i] = a[i], a[n]
 	}
-	return a
+	b := make([]int, l)
+	copy(b, a)
+	rotateSlice(b, r.Intn(l-2)+1)
+	dual := &dualSorting{keys: a, values: b}
+	sort.Sort(dual)
+	return dual.values
+}
+
+func rotateSlice[T any](a []T, k int) {
+	k %= len(a)
+	// Condition below is added.
+	if k < 0 {
+		k += len(a)
+	}
+	b := make([]T, len(a))
+	copy(b[:k], a[len(a)-k:])
+	copy(b[k:], a[:len(a)-k])
+	copy(a, b)
+}
+
+type dualSorting struct {
+	keys, values []int
+}
+
+var _ sort.Interface = &dualSorting{}
+
+func (d *dualSorting) Len() int {
+	return len(d.keys)
+}
+
+func (d *dualSorting) Less(i, j int) bool {
+	return d.keys[i] < d.keys[j]
+}
+
+func (d *dualSorting) Swap(i, j int) {
+	d.keys[i], d.keys[j] = d.keys[j], d.keys[i]
+	d.values[i], d.values[j] = d.values[j], d.values[i]
 }
--- shuffle_test.go
+++ shuffle_test.go
@@ -1,30 +1,54 @@
 package main
 
 import (
+	"fmt"
 	"math"
 	"testing"
 )
 
+func isInvalid(t *testing.T, a []int) {
+	if hasDuplicates(a) {
+		t.Log(a)
+		t.Fatal("List has duplicates")
+	}
+	if i, y := hasSamePosition(a); y {
+		t.Log(a)
+		t.Fatalf("Number %d should not stay in the same position", i)
+	}
+}
+
+func hasDuplicates(a []int) bool {
+	b := make([]int, len(a))
+	for i := range a {
+		b[a[i]]++
+		if b[a[i]] > 1 {
+			return true
+		}
+	}
+	return false
+}
+
+func hasSamePosition(a []int) (int, bool) {
+	for k, v := range a {
+		if k == v {
+			return k, true
+		}
+	}
+	return 0, false
+}
+
 func TestShuffledIntSlice(t *testing.T) {
 	for i := int64(0); i < math.MaxInt16; i++ {
-		a := ShuffledIntSlice(16, i)
-		for k, v := range a {
-			if k == v {
-				t.Log(a)
-				t.Fatal("Number should not stay in the same position")
-			}
-		}
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			a := ShuffledIntSlice(16, i)
+			isInvalid(t, a)
+		})
 	}
 }
 
 func FuzzShuffledIntSlice(f *testing.F) {
 	f.Fuzz(func(t *testing.T, seed int64) {
 		a := ShuffledIntSlice(16, seed)
-		for k, v := range a {
-			if k == v {
-				t.Log(a)
-				t.Fatal("Number should not stay in the same position")
-			}
-		}
+		isInvalid(t, a)
 	})
 }
